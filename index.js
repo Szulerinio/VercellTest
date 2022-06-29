5f7f3e345ce8fa80e70d9c660e0c6870477dec6e
Szulerinio <szymonkozlowski48@gmail.com>
2022-06-30 00:39:32 +0200

    change build script

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var fs = require("fs");
 
 // writeFile function with filename, content and callback function
-fs.writeFile("newfile.txt", "Learn Node FS module", function (err) {
+fs.writeFile("public/newfile.txt", "Learn Node FS module", function (err) {
   if (err) throw err;
   console.log("File is created successfully.");
 });
